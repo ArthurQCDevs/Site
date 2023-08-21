e654e3783814f7d2cfd55367b48ce1ac6424d51f
ArthurQCDevs <126675124+ArthurQCDevs@users.noreply.github.com>
2023-08-21 20:52:44 -0300

    Add files via upload

---
+++ Meu site/itens_portfolio/Cofrinho/cofrinho/src/Real.java
@@ -0,0 +1,13 @@
+public class Real extends Moeda {
+    public static final String NOME = "Real";
+    public static final String SIMBOLO = "R$";
+    public static final int[] VALORES_POSSIVEIS = { 5, 10, 25, 50 };
+
+    Real(int valor) throws MoedaInvalida {
+        super(NOME, SIMBOLO, valor, VALORES_POSSIVEIS);
+    }
+
+    public double valorEmReais() {
+        return VALOR * 0.01;
+    }
+}
